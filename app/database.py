a788fc603d6fbd4e1e6b984d86d31c321dcea444
isaac4124 <sugussanchez31@gmail.com>
2026-03-04 23:18:12 +0100

    "Resuelve conflictos de git y corrige errores en SQLAlchemy".

--- app/database.py
+++ app/database.py
@@ -1,14 +1,9 @@
 from sqlalchemy import create_engine
-
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL="postgresql://postgres:ISAAC@localhost:5432/ecomerce_db"
 
 engine = create_engine(DATABASE_URL)
-sessionmaker = sessionmaker(autocomit=False, autoflush=False, bind=engine)
-<<<<<<< HEAD
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-=======
-Base = declarative_base
->>>>>>> b9f31fd139b225c59d85988d151aece48687f3ba
--- app/models.py
+++ app/models.py
@@ -1,52 +1,25 @@
-<<<<<<< HEAD
-from sqlalchemy import Column ,Integer,String,Float,Boolean,ForeignKey
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 
 class Categoria(Base):
     __tablename__ ="categorias"
-    id = Column(Integer,primary_key = True, index=True)
-    nombre = Column(String, unique=True,index=True)
-=======
-from sqlalchemy import column ,Integer,String,Float,Boolean,ForeignKey
-from sqlalchemy.orm import relationship
-from database import Base
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, unique=True, index=True)
+    productos = relationship("Producto", back_populates="categoria")
 
-class Categotias(Base):
-    __tablename__ ="categorias"
-    id = column(Integer,primary_key = True, index=True)
-    nombre = column(String, unique=True,index=True)
->>>>>>> b9f31fd139b225c59d85988d151aece48687f3ba
-    productos = relationship("Producto", back_populates="categorias")
-
-class Productos(Base):
+class Producto(Base):
     __tablename__ ="Productos"
-<<<<<<< HEAD
-    id = Column(Integer,primary_key = True, index=True)
-    nombre=Column(String, index=True)
-    precio = Column(float)
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, index=True)
+    precio = Column(Float)
     en_stock = Column(Boolean, default=True)
     categoria_id = Column(Integer, ForeignKey("categorias.id"))
-=======
-    id = column(Integer,primary_key = True, index=True)
-    nombre=column(String, index=True)
-    precio = column(float)
-    en_stock = column(Boolean, default=True)
-    categoria_id = column(Integer, ForeignKey("categorias.id"))
->>>>>>> b9f31fd139b225c59d85988d151aece48687f3ba
-    Categotia = relationship("Categoria", back_populates="Productos")
+    categoria = relationship("Categoria", back_populates="productos")
 
 class Usuario(Base):
     __tablename__ ="Usuario"
-<<<<<<< HEAD
-    id = Column(Integer,primary_key = True, index=True)
-    nombre = Column(String, unique=True,index=True)
-    email = Column(String,unique=True,index= True)
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-=======
-    id = column(Integer,primary_key = True, index=True)
-    nombre = column(String, unique=True,index=True)
-    email = column(String,unique=True,index= True)
-    hashed_password = column(String)
->>>>>>> b9f31fd139b225c59d85988d151aece48687f3ba
-
